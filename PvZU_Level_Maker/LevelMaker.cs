ef4c8b374985fc998aab11009e3d0ad6c444bbd7
Gamer-On-Coding <thegamingboss204@gmail.com>
2024-07-31 19:31:34 +0530

    laptop setup

--- PvZU_Level_Maker/LevelMaker.cs
+++ PvZU_Level_Maker/LevelMaker.cs
@@ -5,8 +5,8 @@ namespace PvZU_Level_Maker
     public partial class LevelMaker : Form
     {
 
-        public static readonly World[] worlds = [Declare.EM, Declare.AE, Declare.PS, Declare.WW, Declare.KW];
-        public static readonly FirstRewardType[] firstRewardTypes = [Declare.none];
+        public static readonly World[] worlds = { Declare.EM, Declare.AE, Declare.PS, Declare.WW, Declare.KW };
+        public static readonly FirstRewardType[] firstRewardTypes = { Declare.none };
 
         public LevelMaker()
         {
