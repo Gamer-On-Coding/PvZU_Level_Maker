e73f9aa25a1f5ba215f56072a6b7e1ea06e172d3
Gamer-On-Coding <87073110+Gamer-On-Coding@users.noreply.github.com>
2025-06-14 14:49:22 +0530

    1.3

--- obj/Release/net8.0-windows/PvZU_Level_Maker.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/PvZU_Level_Maker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PvZU_Level_Maker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8865aab6fb03fb7e80e1ff3963e1f1a6662211")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b051cb3acf79cfb837a2a6b655924ec6f94f2784")]
 [assembly: System.Reflection.AssemblyProductAttribute("PvZU_Level_Maker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PvZU_Level_Maker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
